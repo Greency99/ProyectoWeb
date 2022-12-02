0260061f9109d25a6db01e06a76ac592bda5782b
Greency99 <Greency99@users.noreply.github.com>
2022-12-01 18:38:10 -0600

    final changes

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const express = require("express");
 const app = express();
 const config = require("dotenv").config();
 const router = require("./src/routes/router");
-const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
 
-app.use(cors);
-
-app.use("/", router);
+app.use("/api", router);
 
 
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -6,8 +6,8 @@ const calendarRouter = require("./calendar.route");
 const loginRouter = require("./login.route");
 
 //define use of route
-router.use("/api/users", userRouter);
-router.use("/api/calendar", calendarRouter);
+router.use("/users", userRouter);
+router.use("/calendar", calendarRouter);
 router.use("/login", loginRouter);
 
 module.exports = router;
